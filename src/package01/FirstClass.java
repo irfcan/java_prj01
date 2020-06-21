8de06ccb243273d0b68a0cf92165d12002883884
irfcan <irfancanan@gmail.com>
2020-06-21 22:14:23 +0300

    ikinci commit local repository'den

--- src/package01/FirstClass.java
+++ src/package01/FirstClass.java
@@ -5,6 +5,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 		
 		System.out.println("Ýlk commit from local repository");
+		System.out.println("Ýkinci commit from local repository");
 
 	}
 
