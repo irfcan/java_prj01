10cbd5a270fe0c8863c58451f8cebd92e3df58d1
Irfan Canan <irfancanan@gmail.com>
2020-06-22 07:19:37 +0300

    Update FirstClass.java
    ilk commit from remote repo

--- src/package01/FirstClass.java
+++ src/package01/FirstClass.java
@@ -4,8 +4,10 @@ public class FirstClass {
 
 	public static void main(String[] args) {
 		
-		System.out.println("›lk commit from local repository");
-		System.out.println("›kinci commit from local repository");
+		System.out.println("√ùlk commit from local repository");
+		System.out.println("√ùkinci commit from local repository");
+		
+		System.out.println("Son commit from remote repository");
 
 	}
 
